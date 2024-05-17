2208a7d4f1a2bc340f5f6519aebce6218cfec4da
rdang2 <robin.dang@booking.com>
2024-05-17 16:17:11 +0200

    cp setup

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Reader r = new Reader();
+        int n = r.nextInt();
+        System.out.println(n);
+    }
+}
