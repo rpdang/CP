269683f97dbfed05a7c007b26b2908914f84d682
rdang2 <robin.dang@booking.com>
2024-05-21 22:45:35 +0200

    add nextGrid

--- Main.java
+++ Main.java
@@ -8,8 +8,15 @@ public class Main {
 
     void solve() throws Exception {
         int a = nextInt(), b = nextInt();
-        System.out.println(a);
-        System.out.println(b);
+        int sum = 0;
+        for (int i = 0; i < 7; i++) {
+            sum += i == 6 ? b : a;
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().run();
     }
 
     void run() throws Exception {
@@ -19,10 +26,6 @@ public class Main {
         solve();
         out.flush();
         System.err.println(System.currentTimeMillis() - s + "ms");
-    }
-
-    public static void main(String[] args) throws Exception {
-        new Main().run();
     }
 
     byte[] inbuf = new byte[1024];
@@ -40,33 +43,6 @@ public class Main {
         if (b == 0)
             return a;
         return gcd(b, a % b);
-    }
-
-    private int readByte() {
-        if (lenbuf == -1)
-            throw new InputMismatchException();
-        if (ptrbuf >= lenbuf) {
-            ptrbuf = 0;
-            try {
-                lenbuf = is.read(inbuf);
-            } catch (IOException e) {
-                throw new InputMismatchException();
-            }
-            if (lenbuf <= 0)
-                return -1;
-        }
-        return inbuf[ptrbuf++];
-    }
-
-    private boolean isSpaceChar(int c) {
-        return !(c >= 33 && c <= 126);
-    }
-
-    private int skip() {
-        int b;
-        while ((b = readByte()) != -1 && isSpaceChar(b))
-            ;
-        return b;
     }
 
     private String nextString() {
@@ -130,119 +106,49 @@ public class Main {
             b = readByte();
         }
     }
-}
 
-/*
- * class Reader {
- * final private int BUFFER_SIZE = 1 << 16;
- * private DataInputStream din;
- * private byte[] buffer;
- * private int bufferPointer, bytesRead;
- * 
- * public Reader() {
- * din = new DataInputStream(System.in);
- * buffer = new byte[BUFFER_SIZE];
- * bufferPointer = bytesRead = 0;
- * }
- * 
- * public Reader(String file_name) throws IOException {
- * din = new DataInputStream(
- * new FileInputStream(file_name));
- * buffer = new byte[BUFFER_SIZE];
- * bufferPointer = bytesRead = 0;
- * }
- * 
- * public String readLine() throws IOException {
- * byte[] buf = new byte[64]; // line length
- * int cnt = 0, c;
- * while ((c = read()) != -1) {
- * if (c == '\n') {
- * if (cnt != 0) {
- * break;
- * } else {
- * continue;
- * }
- * }
- * buf[cnt++] = (byte) c;
- * }
- * return new String(buf, 0, cnt);
- * }
- * 
- * public int nextInt() throws IOException {
- * int ret = 0;
- * byte c = read();
- * while (c <= ' ') {
- * c = read();
- * }
- * boolean neg = (c == '-');
- * if (neg)
- * c = read();
- * do {
- * ret = ret * 10 + c - '0';
- * } while ((c = read()) >= '0' && c <= '9');
- * 
- * if (neg)
- * return -ret;
- * return ret;
- * }
- * 
- * public long nextLong() throws IOException {
- * long ret = 0;
- * byte c = read();
- * while (c <= ' ')
- * c = read();
- * boolean neg = (c == '-');
- * if (neg)
- * c = read();
- * do {
- * ret = ret * 10 + c - '0';
- * } while ((c = read()) >= '0' && c <= '9');
- * if (neg)
- * return -ret;
- * return ret;
- * }
- * 
- * public double nextDouble() throws IOException {
- * double ret = 0, div = 1;
- * byte c = read();
- * while (c <= ' ')
- * c = read();
- * boolean neg = (c == '-');
- * if (neg)
- * c = read();
- * 
- * do {
- * ret = ret * 10 + c - '0';
- * } while ((c = read()) >= '0' && c <= '9');
- * 
- * if (c == '.') {
- * while ((c = read()) >= '0' && c <= '9') {
- * ret += (c - '0') / (div *= 10);
- * }
- * }
- * 
- * if (neg)
- * return -ret;
- * return ret;
- * }
- * 
- * private void fillBuffer() throws IOException {
- * bytesRead = din.read(buffer, bufferPointer = 0,
- * BUFFER_SIZE);
- * if (bytesRead == -1)
- * buffer[0] = -1;
- * }
- * 
- * private byte read() throws IOException {
- * if (bufferPointer == bytesRead)
- * fillBuffer();
- * return buffer[bufferPointer++];
- * }
- * 
- * public void close() throws IOException {
- * if (din == null)
- * return;
- * din.close();
- * }
- * }
- */
+    private int[][] nextGrid() {
+        // Read the number of rows and columns
+        int rows = nextInt();
+        int cols = nextInt();
+
+        // Initialize the grid
+        int[][] grid = new int[rows][cols];
+
+        // Read each cell of the grid
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                grid[i][j] = nextInt();
+            }
+        }
+
+        return grid;
+    }
+
+    private int readByte() {
+        if (lenbuf == -1)
+            throw new InputMismatchException();
+        if (ptrbuf >= lenbuf) {
+            ptrbuf = 0;
+            try {
+                lenbuf = is.read(inbuf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (lenbuf <= 0)
+                return -1;
+        }
+        return inbuf[ptrbuf++];
+    }
+
+    private boolean isSpaceChar(int c) {
+        return !(c >= 33 && c <= 126);
+    }
+
+    private int skip() {
+        int b;
+        while ((b = readByte()) != -1 && isSpaceChar(b))
+            ;
+        return b;
+    }
+}
